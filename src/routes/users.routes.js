6fb918854c94fc0e1d9ca519e59d3f55db1c9191
WalterBvxd <samuelgomezmonroy@gmail.com>
2022-06-03 14:56:42 -0600

    confirmar

---
+++ src/routes/users.routes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const router = Router();
+
+const { renderSignUpForm, renderSigninForm, signup, signin, logout } = require('../controllers/users.controller')
+
+router.get('/signup', renderSignUpForm);
+
+router.post('/signup', signup);
+
+router.get('/signin', renderSigninForm);
+
+router.post('/signin', signin);
+
+router.get('/logout', logout);
+
+module.exports = router;
